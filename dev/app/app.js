9e6bc4d6178765b56118c339ad4b379f6255cf81
FazioNico <contact@nicolasfazio.ch>
2016-09-01 12:24:05 +0200

    initial commit

---
+++ dev/app/app.js
@@ -0,0 +1,7 @@
+/**
+* @Author: Nicolas Fazio <webmaster-fazio>
+* @Date:   01-09-2016
+* @Email:  contact@nicolasfazio.ch
+* @Last modified by:   webmaster-fazio
+* @Last modified time: 01-09-2016
+*/
